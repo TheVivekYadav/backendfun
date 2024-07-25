aa2b37e78f6eaa263bb4dcc2a4403b2f59d54c3b
thevivekyadav <vivekyadavclasher@gmail.com>
2024-07-25 11:47:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/twitter', (req, res) => {
+  res.send('Twitter');
+});
+
+app.get('/login', (req, res) => {
+  res.send('<h1>Please Login at chai aur code </h1>');
+});
+
+app.get('/youtube', (req, res) => {
+  res.send('<h2>Chai Aur Code</h2>');
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
